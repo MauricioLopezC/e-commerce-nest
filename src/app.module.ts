79db24d7b2eaee56e94b28c5c0ff34a5c041d5b0
= <=>
2025-03-17 16:31:54 -0300

    start statistics module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { CoreModule } from './core/core.module';
 import { SearchModule } from './search/search.module';
 import { ProductsCoreModule } from './products-core/products-core.module';
+import { StatisticsModule } from './statistics/statistics.module';
 
 @Module({
-  imports: [ProductsCoreModule, UsersModule, AuthModule, PrismaModule, CoreModule, SearchModule],
+  imports: [ProductsCoreModule, UsersModule, AuthModule, PrismaModule, CoreModule, SearchModule, StatisticsModule],
   controllers: [AppController],
   providers: [AppService],
 })
