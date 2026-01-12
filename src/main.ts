76d4e2001ca1324573df69423211b0b050125a2b
Mauricio Lopez <mauroagustin.lopez.456@gmail.com>
2026-01-12 13:25:09 -0300

    feat: add helmet middleware

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,7 @@ async function bootstrap() {
   );
 
   app.enableShutdownHooks();
+  app.use(helmet());
   app.use(cookieParser());
   app.enableCors({
     origin: ['http://localhost:8080'], //nextjs frontend
